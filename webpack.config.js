19a20bc5b8e34b79cdfc319fab7eb1b4cb98b654
Kusal KC <kckusal99@gmail.com>
2021-06-08 07:31:43 +0300

    exclude React & moment from app bundle

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = (config, context) => {
   const nrwlConfig = nrwlWebpack(config);
 
   return merge(nrwlConfig, {
+    externals: [
+      {
+        /** These will be supplied in the client-side externally, so don't bundle them. */
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        moment: 'moment',
+      },
+    ],
     plugins: [new BundleAnalyzerPlugin()],
   });
 };
