373fdafa261dcc17b9b65f74417b618a297de8e0
Kusal KC <kckusal99@gmail.com>
2021-06-08 08:10:21 +0300

    implement route-level lazy loading

--- apps/myapp/src/app/RoutesLoader.tsx
+++ apps/myapp/src/app/RoutesLoader.tsx
@@ -1,14 +1,17 @@
+import { lazy, Suspense } from 'React'
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Contact from './pages/Contact';
+const Home = lazy(() =>import('./pages/Home'));
+const Contact = lazy(() =>import('./pages/Contact'));
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route component={Home} exact path="/" />
-      <Route component={Contact} path="/contact" />
-    </Switch>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Switch>
+        <Route component={Home} exact path="/" />
+        <Route component={Contact} path="/contact" />
+      </Switch>
+    </Suspense>
   );
 };
 
